05c2a3c7178d0db23bbcd5c6ff24ac8f3c1ebfb1
Alexandru Olaru <alxolr@gmail.com>
2020-08-16 22:50:25 +0300

    Added the sigil letter removal

--- sigil/src/main.rs
+++ sigil/src/main.rs
@@ -3,14 +3,10 @@ use linked_hash_set::LinkedHashSet;
 
 fn main() {
     let intent = std::env::args().nth(1).expect("no pattern given");
-    let vovewls = vec!['A', 'E', 'I', 'O', 'U']
-        .into_iter()
-        .collect::<LinkedHashSet<char>>();
 
     let sigil = intent
         .to_uppercase()
         .chars()
-        .filter(|c| !vovewls.contains(c))
         .collect::<LinkedHashSet<char>>()
         .into_iter()
         .collect::<String>();

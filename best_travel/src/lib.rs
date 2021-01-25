9d80393b1283279d94638ae2e3f4f5004c93f07a
Alexandru Olaru <alxolr@gmail.com>
2021-01-25 23:51:45 +0200

    Wow solution for the choose_best_sum problem

--- best_travel/src/lib.rs
+++ best_travel/src/lib.rs
@@ -1,9 +1,13 @@
-/**
- * dynamic problems, usually are solved using memoization technique
- */
+extern crate itertools;
+use itertools::Itertools;
 
 fn choose_best_sum(t: i32, k: i32, ls: &Vec<i32>) -> i32 {
-    168
+    ls.iter()
+        .combinations(k as usize)
+        .map(|c| c.into_iter().sum::<i32>())
+        .filter(|s| s <= &t)
+        .max()
+        .unwrap_or(-1)
 }
 
 #[cfg(test)]
@@ -16,6 +20,7 @@ mod tests {
         let ts = &vec![50];
         testing(163, 3, ts, -1);
         let ts = &vec![91, 74, 73, 85, 73, 81, 87];
+        testing(331, 4, ts, 331);
         testing(230, 3, ts, 228);
         testing(331, 2, ts, 178);
     }

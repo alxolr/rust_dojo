14d76ae17b0e0d300ffefffc49751861dec86d75
Alexandru Olaru <alxolr@gmail.com>
2024-06-07 14:47:19 +0300

    Solved the problem of the day replace words 648

--- replace_words_648/src/lib.rs
+++ replace_words_648/src/lib.rs
@@ -28,60 +28,80 @@ impl Trie {
     }
 
     fn insert(&mut self, word: &str) {
+        // get the root node as a pointer called current
+        // iterate through the trie data structure, getting next child
         let mut current = &mut self.root;
 
         for ch in word.chars() {
+            // for each character in the word we check if we already have a trie node
+            // started at this char, if yes then we continue if no then we start a new node
             let child = current
                 .children
                 .entry(ch)
                 .or_insert(Box::new(TrieNode::new()));
 
+            // we navigate through the trie tree structure using the current/child pair
             current = child;
         }
 
+        // mark the node at the last letter to be true
         current.is_end = true;
     }
 
     fn shortest_root(&self, word: &str) -> String {
-        let mut result = "".to_string();
+        // create a buffer to store the progress of already found characters
+        let mut buffer = String::new();
+
+        // use the same current/child way to navigate through the (trie) tree
         let mut current = &self.root;
-        let mut is_prefix = true;
 
+        // check all the chars in current word
         for ch in word.chars() {
-            result.push(ch);
+            // if we found a letter in the trie we append it to the existing buffer
+            if let Some(child) = current.children.get(&ch) {
+                buffer.push(ch);
 
-            if is_prefix {
-                if let Some(child) = current.children.get(&ch) {
-                    if child.is_end {
-                        break;
-                    }
-
-                    current = child;
-                } else {
-                    is_prefix = false;
+                // once the node is an end word we need to return accumulated buffer as this being the
+                // shortest root for the word
+                if child.is_end {
+                    return buffer;
                 }
+
+                current = child;
+            } else {
+                // if the letter was not found this means there is no prefix root for this word
+                // return an empty string
+                return String::new();
             }
         }
 
-        result
+        return buffer;
     }
 }
 
 impl Solution {
     pub fn replace_words(dictionary: Vec<String>, sentence: String) -> String {
         let mut trie = Trie::new();
-        let mut result: Vec<_> = vec![];
+        let words: Vec<_> = sentence.split_whitespace().collect();
+        let mut result: Vec<_> = Vec::with_capacity(words.len());
 
+        // Add all words in the dictioary to the Trie Data structure
         for word in dictionary {
             trie.insert(&word);
         }
 
-        for word in sentence.split_whitespace() {
-            let word = trie.shortest_root(word);
+        for word in words {
+            // get the shortest root, output will be either empty string or a word from the dictionary
+            let shortest_root = trie.shortest_root(word);
 
-            result.push(word);
+            if shortest_root.is_empty() {
+                result.push(word.to_string());
+            } else {
+                result.push(shortest_root.to_string());
+            }
         }
 
+        // join all the words to return the new sentence
         result.join(" ")
     }
 }

6e532141f5d7093ed1addc63a499556d2145191d
Alexandru Olaru <alxolr@gmail.com>
2024-12-01 01:29:11 +0200

    Added the template for day 1

---
+++ aoc_2024/day_1/src/error.rs
@@ -0,0 +1,21 @@
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Error {
+    FailReadFile(std::io::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::FailReadFile(value)
+    }
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self}")
+    }
+}
+
+impl std::error::Error for Error {}
---
+++ aoc_2024/day_1/src/solution.rs
@@ -0,0 +1,26 @@
+use crate::error::Result;
+pub struct Solution;
+
+impl Solution {
+    pub fn part_1(input: &str) -> Result<i32> {
+        Ok(0)
+    }
+
+    pub fn part_2(input: &str) -> Result<i32> {
+        Ok(0)
+    }
+}
+
+mod tests {
+    #[test]
+    fn test_part_1() {
+        let input = "";
+        assert_eq!(super::Solution::part_1(input).unwrap(), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "";
+        assert_eq!(super::Solution::part_2(input).unwrap(), 0);
+    }
+}

4e7e9bf68958be91af32acf76da54f4c6e2722ea
Alexandru Olaru <alxolr@gmail.com>
2024-12-07 15:22:34 +0200

    Added comments to my solution

--- aoc_2024/day_7/src/solution.rs
+++ aoc_2024/day_7/src/solution.rs
@@ -2,6 +2,9 @@ use crate::error::Result;
 pub struct Solution;
 
 impl Solution {
+    // This would be the common part for the both solution where we outline the main idea and algorithm
+    // We parse the lines first get the target_value, and the operands that we need to play with
+    // We check if our parsed line is valid then we return the target_value to be summed.
     fn common<F>(input: &str, is_valid: F) -> Result<i64>
     where
         F: Fn(i64, &Vec<i64>) -> bool,
@@ -31,37 +34,46 @@ impl Solution {
     }
 }
 
+fn is_valid_part_1(target_value: i64, operands: &Vec<i64>) -> bool {
+    // This part is a bit more complex to understand but the idea here is
+    // We return a list of possible accumulated values
+    let get_accumulated_values = |index: usize, accumulated_value: i64, operand_value| {
+        // We return an options of possible next values cause we need to handle overflows
+        let possible_accumulated_values = vec![
+            // For the check options, we get the already accumulated and the current operand value
+            accumulated_value.checked_add(operand_value),
+            // For the multiplication, we need to check for the first one to not have multiplication by zero
+            // So if it's the first one we just return the current operand value
+            if index == 0 {
+                Some(operand_value)
+            } else {
+                accumulated_value.checked_mul(operand_value)
+            },
+        ];
+
+        possible_accumulated_values
+    };
+
+    // We call the recursive function, starting at index 0, and accumulator zero
+    check_accumulated_operands(0, 0, target_value, operands, get_accumulated_values)
+}
+
 fn is_valid_part_2(target_value: i64, operands: &Vec<i64>) -> bool {
     let get_accumulated_values = |index: usize, accumulated_value: i64, operand_value| {
-        let next_accumulated_values = vec![
+        let possible_accumulated_values = vec![
             accumulated_value.checked_add(operand_value),
             if index == 0 {
                 Some(operand_value)
             } else {
                 accumulated_value.checked_mul(operand_value)
             },
+            // We add the concatenation posibility
             format!("{}{}", accumulated_value, operand_value)
                 .parse::<i64>()
                 .ok(),
         ];
 
-        next_accumulated_values
-    };
-    check_accumulated_operands(0, 0, target_value, operands, get_accumulated_values)
-}
-
-fn is_valid_part_1(target_value: i64, operands: &Vec<i64>) -> bool {
-    let get_accumulated_values = |index: usize, accumulated_value: i64, operand_value| {
-        let next_accumulated_values = vec![
-            accumulated_value.checked_add(operand_value),
-            if index == 0 {
-                Some(operand_value)
-            } else {
-                accumulated_value.checked_mul(operand_value)
-            },
-        ];
-
-        next_accumulated_values
+        possible_accumulated_values
     };
     check_accumulated_operands(0, 0, target_value, operands, get_accumulated_values)
 }
@@ -80,11 +92,15 @@ where
     let next_accumulated_values =
         get_next_accumulated_values(index, accumulated_value, operands[index]);
 
+    // This is the finish line, when our index is the last
+    // We check if any of the accumulated values is equal to our target
     if index == operands.len() - 1 {
         next_accumulated_values
             .iter()
             .any(|&value| value == Some(target_value))
     } else {
+        // In the other cases we go recursively, by checking all possible operators
+        // and we increment the index
         next_accumulated_values.iter().any(|&value| {
             value.map_or(false, |v| {
                 check_accumulated_operands(

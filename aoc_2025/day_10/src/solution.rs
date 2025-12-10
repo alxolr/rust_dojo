62195c6d77b17e11a3513bbcd14492265a875c43
Alexandru Olaru <alxolr@gmail.com>
2025-12-10 15:02:27 +0200

    Created a generic state machine counter

--- aoc_2025/day_10/src/solution.rs
+++ aoc_2025/day_10/src/solution.rs
@@ -1,43 +1,56 @@
-use std::collections::{HashSet, VecDeque};
+use std::{
+    collections::{HashSet, VecDeque},
+    hash::Hash,
+};
 
 use crate::error::Result;
 pub struct Solution;
 
 impl Solution {
     pub fn part_1(input: &str) -> Result<u64> {
+        let xor = |a: &u16, b: &u16| -> u16 { a ^ b };
+
         let answer = parse_input(input)
-            .map(|(state, masks, _)| solve_part_1(state, &masks))
+            .map(|(initial_state, masks)| state_machine(initial_state, 0u16, &masks, xor))
             .sum();
 
         Ok(answer)
     }
 
-    pub fn part_2(input: &str) -> Result<u64> {
+    pub fn part_2(_input: &str) -> Result<u64> {
         let answer = 1;
         Ok(answer)
     }
 }
 
-fn solve_part_1(initial_state: u16, masks: &Vec<u16>) -> u64 {
+// Count the minimum amount of changes
+fn state_machine<T, F>(
+    initial_state: T,
+    target_state: T,
+    transitions: &Vec<T>,
+    state_change: F,
+) -> u64
+where
+    T: Hash + Eq + PartialEq + Clone,
+    F: Fn(&T, &T) -> T,
+{
     // we treat the state as a u16 number, and the masks as well, we will use xor to inverse the bits
     // we will use a breadth first search algorithm to find the number of iterations
     // we go backwards from our state to state ....... -> which is zero
-
     let mut visited_states = HashSet::new();
-    let mut state_machine = VecDeque::<(usize, u16)>::new();
-    state_machine.push_front((0, initial_state));
+    let mut state_machine = VecDeque::<(usize, T)>::new();
+    state_machine.push_front((0, initial_state.clone()));
     visited_states.insert(initial_state);
-    let target_state = 0u16;
 
     while let Some((clicks, state)) = state_machine.pop_front() {
         if state == target_state {
             return clicks as u64;
         }
 
-        for mask in masks.iter() {
-            let new_state = state ^ mask;
+        for mask in transitions.iter() {
+            let new_state = state_change(&state, mask);
             if !visited_states.contains(&new_state) {
-                visited_states.insert(new_state);
+                visited_states.insert(new_state.clone());
                 state_machine.push_back((clicks + 1, new_state));
             }
         }
@@ -46,7 +59,7 @@ fn solve_part_1(initial_state: u16, masks: &Vec<u16>) -> u64 {
     panic!("Expected to finish by now")
 }
 
-fn parse_input(input: &str) -> impl Iterator<Item = (u16, Vec<u16>, Vec<u16>)> + use<'_> {
+fn parse_input(input: &str) -> impl Iterator<Item = (u16, Vec<u16>)> + use<'_> {
     input.lines().map(|line| {
         // let mut parts = line.trim().split_whitespace();
         let line_parts = line.trim().split_whitespace().collect::<Vec<_>>();
@@ -62,13 +75,6 @@ fn parse_input(input: &str) -> impl Iterator<Item = (u16, Vec<u16>, Vec<u16>)> +
             .collect::<String>();
 
         let masks = line_parts.iter().skip(1).take(count - 2);
-
-        let joltages = line_parts[count - 1]
-            .replace("{", "")
-            .replace("}", ",")
-            .split(",")
-            .flat_map(|joltage| joltage.parse::<u16>())
-            .collect();
 
         let state = u16::from_str_radix(&state, 2).unwrap_or_default();
         let masks = masks
@@ -90,7 +96,7 @@ fn parse_input(input: &str) -> impl Iterator<Item = (u16, Vec<u16>, Vec<u16>)> +
             })
             .collect::<Vec<u16>>();
 
-        (state, masks, joltages)
+        (state, masks)
     })
 }
 
